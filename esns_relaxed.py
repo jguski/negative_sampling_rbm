c2cdefb25c25313a599835b0e54a38be9a366af6
Jannis Guski <jannis.guski@rub.de>
2022-06-04 11:23:46 +0200

    incorporated ESNS code from publication

--- esns_relaxed.py
+++ esns_relaxed.py
@@ -2,13 +2,16 @@
 counting number of shared relations instead of relation + target pairs for similarity measure"""
 
 import torch
+import scipy
+from scipy import sparse
+import numpy as np
 
 from pykeen.sampling import BernoulliNegativeSampler
-from pykeen.typing import COLUMN_HEAD, COLUMN_TAIL, MappedTriples
+from pykeen.typing import COLUMN_HEAD, COLUMN_TAIL, COLUMN_RELATION, MappedTriples
 from pykeen.models import Model
 
 import os
-import pickle
+import sys
 import logging
 from tqdm import tqdm
 
@@ -65,23 +68,40 @@ class ESNSRelaxed(BernoulliNegativeSampler):
         logger = logging.getLogger(self.__class__.__name__)
         logger.setLevel("INFO")
 
-        # handling of pickle files with inverse indices
+        # handling of npz files with inverse indices
         if not os.path.exists(self.index_path):
             os.makedirs(self.index_path)
 
-        filename_base = self.index_path + "/" + self.__class__.__name__ + "_" + self.similarity_function.__name__ + "_k" + str(self.index_column_size)
-        if not (os.path.exists(filename_base + "_h.pkl") and os.path.exists(filename_base + "_t.pkl")):
-            logger.info("Creating EII {}_h.pkl".format(filename_base))
-            self.eii_h = self._create_eii(COLUMN_HEAD)#.view(-1, self.index_column_size*self.num_entities)
-            pickle.dump(self.eii_h, open(filename_base + "_h.pkl", 'wb'))
-            logger.info("Creating EII {}_t.pkl".format(filename_base))
-            self.eii_t = self._create_eii(COLUMN_TAIL)#.view(-1, self.index_column_size*self.num_entities)
-            pickle.dump(self.eii_t, open(filename_base + "_t.pkl", 'wb'))
+        filename_base = self.index_path + "/" + self.__class__.__name__ + "_" + self.similarity_function.__name__
+        if not (os.path.exists(filename_base + "_h.npz") and os.path.exists(filename_base + "_t.npz")):
+            logger.info("Creating EII {}_h.npz".format(filename_base))
+            self.eii_h = self._create_eii(COLUMN_HEAD)
+            scipy.sparse.save_npz(filename_base + '_h.npz', self.eii_h)
+            logger.info("Creating EII {}_t.npz".format(filename_base))
+            self.eii_t = self._create_eii(COLUMN_TAIL)
+            scipy.sparse.save_npz(filename_base + '_t.npz', self.eii_t)
         else: 
-            logger.info("Loading EII {}_h.pkl".format(filename_base))
-            self.eii_h = pickle.load(open(filename_base + "_h.pkl", 'rb'))
-            logger.info("Loading EII {}_t.pkl".format(filename_base))
-            self.eii_t = pickle.load(open(filename_base + "_t.pkl", 'rb'))
+            logger.info("Loading EII {}_h.npz".format(filename_base))
+            self.eii_h = scipy.sparse.load_npz(filename_base + '_h.npz').tolil()
+            logger.info("Loading EII {}_t.npz".format(filename_base))
+            self.eii_t = scipy.sparse.load_npz(filename_base + '_t.npz').tolil()
+
+        # get top self.index_column_size similar entities
+        if self.index_column_size != 0:
+            ent = 0
+            for data, row in zip(self.eii_h.data, self.eii_h.rows):
+                if self.index_column_size < len(row):
+                    d, r = zip(*sorted(zip(data, row), reverse=True)[:self.index_column_size])
+                    self.eii_h.data[ent] = list(d)
+                    self.eii_h.rows[ent] = list(r)
+                ent +=1
+            ent = 0
+            for data, row in zip(self.eii_t.data, self.eii_t.rows):
+                if self.index_column_size < len(row):
+                    d, r = zip(*sorted(zip(data, row), reverse=True)[:self.index_column_size])
+                    self.eii_t.data[ent] = list(d)
+                    self.eii_t.rows[ent] = list(r)
+                ent += 1
 
     def _create_eii(self, column: int) -> None:
         """
@@ -92,8 +112,8 @@ class ESNSRelaxed(BernoulliNegativeSampler):
        
         # initialize (3 x self.index_column_size x num_entities) EII tensor: Contains 3-rowed matrix for each entity, 
         # corresponding to index of the entity (row 0), indices (row 1) and similarity values (row 2) of top self.index_column_size entities
-        eii = torch.zeros(3, self.index_column_size, self.num_entities, device=self.model.device)
-
+        #eii = torch.zeros(3, self.index_column_size, self.num_entities, device=self.model.device)
+        eii = sparse.csr_matrix((self.num_entities, self.num_entities)).tolil()
         # create a |E|x|R| 2D tensor that is 1 where there is a relation, and 0 where there is not
         # store all unique cominbations of (head/tail) entities with relations (to be used as indices for sparse tensor)
         relation_indices = self.mapped_triples.index_select(dim=1, index=torch.tensor([column,1], device=self.model.device)).unique(dim=0)
@@ -105,25 +125,24 @@ class ESNSRelaxed(BernoulliNegativeSampler):
         for i in tqdm(range(self.num_entities)):
             # compute similarity values of all e_j with e_i
             similarities = self.similarity_function(relation_matrix=relation_matrix, mapped_triples=self.mapped_triples, head_or_tail=column, entity_id=i)
-            # set similarity of e_i with itself to 0
-            similarities[i] = 0 
-            # save indices of top self.index_column_size similar e_js
-            topk = similarities.topk(min(self.num_entities, self.index_column_size))
-            # save to i-th EII tensor slice: x-coordinate (i), y-coordinate (topk.indices) and values to be used to be able to create a sparse tensor from this EII
-            eii[0,...,i] = i
-            eii[1,...,i] = topk.indices
-            eii[2,...,i] = topk.values
+            # similarity of an entity to itself should be 0
+            similarities[i] = 0
+            eii[i] = similarities
 
-        return eii
+        return eii.tocsr()
 
-    def _get_best_by_score_fn(self, original_triple: torch.LongTensor, candidates: torch.LongTensor, column: int) -> int:
-        corrupted_triples = original_triple.repeat(candidates.size()).view(-1, 3)
-        corrupted_triples[...,column] = candidates
-        index_of_best_candidate = self.model.score_hrt(corrupted_triples).argmax()
 
-        return candidates[index_of_best_candidate]
+    def _similar_ent(self, original, candidate, index):
+        shape = original.shape
+        original = original.reshape(-1)
+        candidate = candidate.reshape(-1)
 
-    def _esns_replacement(self, batch: torch.LongTensor, index: int, selection: slice, size: int, max_index: int) -> None:
+        eii = (self.eii_h if index == COLUMN_HEAD else self.eii_t)
+        
+        return torch.from_numpy(eii[original, candidate].toarray()).reshape(shape)
+
+
+    def _esns_replacement(self, batch, index, selection, size, max_index):
         """
         Replace a column of a batch of indices by random indices.
         :param batch: shape: `(*batch_dims, d)`
@@ -137,39 +156,73 @@ class ESNSRelaxed(BernoulliNegativeSampler):
         :param max_index:
             the maximum index value at the chosen position
         """
-        # return straight away if size of selection happens to be zero (function would crash otherwise)
-        if size == 0:
-            return
 
-        # create quality sets (similar entities plus random subset of non-similar entities from uniform_sample_set)
-        eii = (self.eii_h if index == COLUMN_HEAD else self.eii_t)
+        head = batch[selection, COLUMN_HEAD]
+        tail = batch[selection, COLUMN_TAIL]
+        rela = batch[selection, COLUMN_RELATION]
+        # uniform generate samples
+        if index == COLUMN_HEAD:
+            # draw self.sampling_size random samples for each triple in selection
+            h_cand = np.random.choice(max_index, (head.shape[0], self.sampling_size))
+            # move ids of original heads to numpy
+            head_idx = head.cpu().numpy()
+            # repeat head id sampling_size times
+            head_i = np.tile(head_idx.reshape(-1, 1), self.sampling_size)
+            probs = self._similar_ent(head_i, h_cand, index)
+            _, h_new = torch.topk(probs, self.q_set_size, dim=-1)
+            h_idx = torch.arange(0, head.shape[0]).type(torch.LongTensor).unsqueeze(1).expand(-1, self.q_set_size)
+            # create array with q_set_size (columns) candidates per triple in selection (rows) and turn into a torch tensor
+            h_rep = h_cand[h_idx, h_new]
+            h_cand = torch.from_numpy(h_rep).type(torch.LongTensor).to(self.model.device)
+            n = head.size(0)
+            
+            tail = tail.unsqueeze(1).expand_as(h_cand).flatten()
+            rela = rela.unsqueeze(1).expand_as(h_cand).flatten()
 
-        # create tensor with one row per triple in selection, containing row from eii corresponding to head or tail value
-        #similar_entities = index_select_sparse(eii, 0, batch[selection,index]).to_dense()
-        indices_similar_entities = eii.index_select(dim=2, index=batch[selection,index]).view(-1, self.index_column_size*size)
-        similar_entities = torch.zeros(size, self.num_entities, device=self.model.device)
-        similar_entities[torch.repeat_interleave(torch.tensor(list(range(size)), device=self.model.device), self.index_column_size), indices_similar_entities[1].long()] = indices_similar_entities[2] 
-        # initialize uniform sample set with zeros
-        uniform_sample_set = torch.zeros((size, self.num_entities), device=self.model.device)
-        # determine the rows to be filled with ones in sample set (just self.sampling_size times the indices of the selection)
-        rows_sample_set = torch.repeat_interleave(torch.tensor(list(range(size)), device=self.model.device), self.sampling_size)
-        # determine the columns to be filled with ones in sample set (self.sampling_size random entity indices for each tuple in selection)
-        columns_sample_set = torch.randint(high=max_index, size=(size*self.sampling_size,), device=self.model.device)
-        # fill uniform_sample_set with ones at the preselected positions
-        uniform_sample_set[rows_sample_set, columns_sample_set] = 1
-        # compute matrix containing only similarity values at positions present in the random sample
-        similar_entities_in_sample = similar_entities*uniform_sample_set
-        # select non-similar entities in random samples: They have to be 0 in similar_entities and 1 in uniform_sample_set
-        nonsimilar_entities_in_sample = torch.where(similar_entities==0, 1, 0)*uniform_sample_set
-        # extract quality candiate sets now: top self.q_set_size similar entities. Nonsimilar entities are weighted with a small value so that 
-        # they have a higher value than entities not in the sample, but a lower than similar entities in the sample
-        # TODO: See if there is a way to shuffle order of the selected nonsimilar entities (atm, some are picked with a higher probability)
-        quality_candidates = (similar_entities_in_sample + nonsimilar_entities_in_sample*(10**-10)).topk(k=self.q_set_size,dim=1,sorted=False)
-        # select best candidate from quality candidates by the KGE model's score function
-        triples=batch[selection]
-        replacement = [self._get_best_by_score_fn(triples[i], quality_candidates.indices[i], index) for i in range(size)]
+            candidate_triples = torch.stack((h_cand.flatten(), rela, tail)).t()
+            scores = self.model.score_hrt(candidate_triples)
+            scores = scores.view(n, -1)
 
-        batch[selection, index] = torch.stack(replacement)
+            probs = scores
+
+            #probs = self.model.prob(h_cand, tail, rela)
+            _, h_new = torch.topk(probs, 1, dim=-1)
+
+            row_idx = torch.arange(0, n).type(torch.LongTensor)
+            if row_idx.size() != h_new.size():
+                row_idx = row_idx.unsqueeze(1).expand_as(h_new)
+            h_idx = h_cand[row_idx, h_new].cpu().numpy()
+            replacement = torch.LongTensor(h_idx).to(self.model.device)
+
+        else:
+            t_cand = np.random.choice(max_index, (head.shape[0], self.sampling_size))
+            tail_idx = tail.cpu().numpy()
+            tail_i = np.tile(tail_idx.reshape(-1, 1), self.sampling_size)
+            probs = self._similar_ent(tail_i, t_cand, index)
+            _, t_new = torch.topk(probs, self.q_set_size, dim=-1)
+            t_idx = torch.arange(0, tail.shape[0]).type(torch.LongTensor).unsqueeze(1).expand(-1, self.q_set_size)
+            t_rep = t_cand[t_idx, t_new]
+            t_cand = torch.from_numpy(t_rep).type(torch.LongTensor).to(self.model.device)
+            n = tail.size(0)
+            head = head.unsqueeze(1).expand_as(t_cand).flatten()
+            rela = rela.unsqueeze(1).expand_as(t_cand).flatten()
+
+            candidate_triples = torch.stack((head, rela, t_cand.flatten())).t()
+
+            scores = self.model.score_hrt(candidate_triples)
+            scores = scores.view(n, -1)
+
+            probs = scores
+
+            _, t_new = torch.topk(probs, 1, dim=-1)
+            row_idx = torch.arange(0, n).type(torch.LongTensor)
+            if row_idx.size() != t_new.size():
+                row_idx = row_idx.unsqueeze(1).expand_as(t_new)
+
+            t_idx = t_cand[row_idx, t_new].cpu().numpy()
+            replacement = torch.LongTensor(t_idx).to(self.model.device)
+        
+        batch[selection, index] = replacement.flatten()
 
     def corrupt_batch(self, positive_batch: torch.LongTensor) -> torch.LongTensor:
         # get number of positive triples in a batch
@@ -182,7 +235,7 @@ class ESNSRelaxed(BernoulliNegativeSampler):
         ) < head_corruption_probability.to(device=self.model.device)
         
         # clone positive batch for corruption (.repeat_interleave creates a copy)
-        negative_batch = positive_batch.view(-1, 3).repeat_interleave(self.num_negs_per_pos, dim=0).to(self.model.device)
+        negative_batch = positive_batch.view(-1, 3).repeat_interleave(self.num_negs_per_pos, dim=0)
         # flatten mask
         head_mask = head_mask.view(-1)
         
@@ -191,6 +244,7 @@ class ESNSRelaxed(BernoulliNegativeSampler):
             # Tails are corrupted if heads are not corrupted
             (COLUMN_TAIL, ~head_mask),
         ):  
+            #self._esns_replacement(
             self._esns_replacement(
                 batch=negative_batch,
                 index=index,

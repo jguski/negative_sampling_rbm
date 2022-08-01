5eb2800d40e60ffdab72e28e66466ccf67c8d999
Jannis Guski <jannis.guski@rub.de>
2022-08-01 15:30:57 +0200

    added shift-based logistic loss function

---
+++ losses/custom_losses.py
@@ -0,0 +1,24 @@
+from pykeen.losses import DeltaPointwiseLoss
+import torch
+from torch.nn import LogSigmoid
+from typing import Any, ClassVar, Mapping
+
+class ShiftLogLoss(DeltaPointwiseLoss):
+
+    hpo_default: ClassVar[Mapping[str, Any]] = dict(
+        shift = dict(type=int, low=0, high=25, step=1)
+    )
+
+    def __init__(self, reduction: str = "sum", shift = 0) -> None:
+        super().__init__(margin=0.0, reduction=reduction, margin_activation=LogSigmoid())
+        self.shift = shift
+
+    def forward(
+        self,
+        logits: torch.FloatTensor,
+        labels: torch.FloatTensor,
+    ) -> torch.FloatTensor:
+
+        return super().forward(logits=logits + self.shift, labels=labels)
+
+    
